71f5aa521db52f78fe2a2c5d25decce9006ff0ca
DontStopPrograming <01dyamond01@gmail.com>
2022-07-26 23:49:40 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,51 +1,47 @@
-import React, { useEffect, useState } from  'react'
+import {BrowserRouter as Router, Route, Routes, Link, NavLink} from 'react-router-dom'
+import About from './Pages/About'
+import Contact from './Pages/Contact'
+import Inicio from './Pages/Inicio'
 import './App.css';
 
+
 function App() {
-  const [valor, setValor] = useState([])
-  const [search, setSearch] = useState({
-    nombre: ''
-  })
-  
-  const apiMostrar = async() => {
-  const data = await fetch('https://pixabay.com/api/?key=27058290-ee9b8ab8787754e59d4d4e22e&q=cafe&per_page=10&')
-  const docJson = await data.json()
-    
-    console.log(docJson)
-    setValor(docJson)
-    setValor(data)
-  }
-
-  useEffect(() =>{
-    apiMostrar()
-  }, [])
-
-  const handleClick = (e) => {
-    console.log('CLICK')
-    
-  }
-
-  const handleChange = (e) => {
-    console.log(e.target.value)
-    setSearch({
-      ...search,
-      [e.target.name] : e.target.value
-    })
-
-  }
-
   return(
     <>
-  <div className="navbar bg-light">
-    <div className="container-fluid">
-    
-      <input type="search" placeholder="busca tu imagen" onChange = {handleChange}  aria-label="Search" name = 'nombre'/> 
-      <button className="btn btn-outline-success" type="submit" onClick = {handleClick} >Search</button>
-      
-    </div>
-  </div>
-    <h3> {search.nombre} - </h3>
+      <Router>
+        <div className = 'contenedor'>
+          <Link to = '/' className = 'btn btn-dark'>
+            INICIO
+          </Link>
+
+          <Link to = '/about' className = 'btn btn-dark'>
+            ABOUT
+          </Link>
+
+          <NavLink to = '/contact' className = 'btn btn-dark' activeClassName = 'active'>
+            CONTACT
+          </NavLink>
+
+        </div>
+
+        <Routes>
+          <Route path = '/contact'>
+            <Contact />
+          </Route>
+
+          <Route path = '/about'>
+            <About />
+          </Route>
+
+          <Route path = '/' exact>
+            <Inicio />
+          </Route>
+
+        </Routes>
+        
+      </Router>
     </>
-  )
-}
+    )
+  }
+
 export default App;
